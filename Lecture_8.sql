e958aed02e4bc420befb9e2b94f017913397333a
Anurag Panda <73139414+anurag596savage@users.noreply.github.com>
2022-07-01 13:31:45 +0530

    Update Lecture_8.sql

--- Lecture_8.sql
+++ Lecture_8.sql
@@ -18,3 +18,27 @@ call student_info_by_age(@records,27);
 
 select @records as total_records;
 
+/*
+
+Procedures used in the code :-
+
+1.
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_student_info`()
+BEGIN
+select * from student;
+END
+
+2.
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_info_by_age`(in age int)
+BEGIN
+select * from student where student.age=age;
+END
+
+3.
+CREATE DEFINER=`root`@`localhost` PROCEDURE `student_info_by_age`(inout records int,in age int)
+BEGIN
+select count(*) into records from student where student.age=age;
+END
+
+*/
+
